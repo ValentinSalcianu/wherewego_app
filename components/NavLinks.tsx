d1e35c0f376897999445e403712dc9f561713ca4
ValentinSalcianu <valentin.salcianu@student.upg-ploiesti.ro>
2024-06-21 19:19:09 +0300

    last version

---
+++ components/NavLinks.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import React from "react";
+import {
+  Home,
+  Search,
+  Compass,
+  Clapperboard,
+  PlusSquare,
+  Heart,
+  MessageCircle,
+  LinkIcon,
+  MountainIcon,
+} from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const links = [
+  {
+    name: "Home",
+    href: "/dashboard",
+    icon: Home,
+  },
+
+  {
+    name: "Search",
+    href: "/dashboard/search",
+    icon: Search,
+    hideOnMobile: true,
+  },
+
+  { name: "Explore", href: "/dashboard/explore", icon: Compass },
+  {
+    name: "Reels",
+    href: "/dashboard/reels",
+    icon: MountainIcon,
+  },
+
+  {
+    name: "Messages",
+    href: "/dashboard/messages",
+    icon: MessageCircle,
+  },
+
+  {
+    name: "Notifications",
+    href: "/dashboard/notifications",
+    icon: Heart,
+    hideOnMobile: true,
+  },
+
+  {
+    name: "Create",
+    href: "/dashboard/create",
+    icon: PlusSquare,
+  },
+];
+
+function NavLinks() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {links.map((link) => {
+        const isActive = pathname === link.href;
+        const LinkIcon = link.icon;
+
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={buttonVariants({
+              variant: isActive ? "secondary" : "ghost",
+              className: cn("navLink", { "hidden md:flex": link.hideOnMobile }),
+              size: "lg",
+            })}
+          >
+            <LinkIcon className="w-6"/>
+
+              <p  className={ ' ${cn("hidden lg:block", {"font-extrabold": isActive, } )} ' } 
+              >
+              
+                {link.name}
+
+              </p>
+            
+
+
+
+          </Link>
+        );
+      })}
+    </>
+  );
+}
+
+export default NavLinks;
